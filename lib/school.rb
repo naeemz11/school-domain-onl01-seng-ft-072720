88fe06028ec0f928a60fe02d39a91cf8f5c128d7
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-25 22:02:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,26 +1,70 @@
-# code here!
 
-class School 
-  attr_accessor :name, :roster 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # code here!
+
+# class School 
+#   attr_accessor :name, :roster 
   
- def initialize(name)
-  @name= name
-  @roster= {}
- end 
+# def initialize(name)
+#   @name= name
+#   @roster= {}
+# end 
 
- def add_student( student_name, grade)
-  roster[grade] ||= []
-    roster[grade] << student_name
- end 
+# def add_student( student_name, grade)
+#   roster[grade] ||= []
+#     roster[grade] << student_name
+# end 
  
- def grade(grade)
-    @roster[grade]
-  end
+# def grade(grade)
+#     @roster[grade]
+#   end
   
-  def sort 
-    @roster.each do |key, value|
-      value.sort!
-    end
-  end
+#   def sort 
+#     @roster.each do |key, value|
+#       value.sort!
+#     end
+#   end
   
- end 
+# end 
